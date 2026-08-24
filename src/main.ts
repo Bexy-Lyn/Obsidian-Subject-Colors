095b77445b6356becd28ba1b6ebf16f98f59eddc
Bexi_Lyn <bexy_lyn@outlook.de>
2026-08-24 15:25:49 +0200

    fix: use renderer

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { Notice, Plugin } from "obsidian";
+
 import {
 	DEFAULT_SETTINGS,
 	SubjectColorSettings,
 	SubjectColorSettingTab,
 } from "./settings";
+import { processThemePlaceholders } from "./renderer";
 
 /**
  * Plugin lifecycle and registration
@@ -14,7 +16,20 @@ export default class SubjectColorPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		this.addSettingTab(new SubjectColorSettingTab(this.app, this));
+		this.addSettingTab(
+			new SubjectColorSettingTab(this.app, this),
+		);
+
+		this.registerMarkdownPostProcessor(
+			(element, context) => {
+				processThemePlaceholders(
+					this.app,
+					element,
+					context,
+					this.settings.tagColors,
+				);
+			},
+		);
 
 		new Notice("Subject Color loaded");
 	}
